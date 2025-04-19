a43064a01cf8e3922207f95dc883c2f9e93fbb04
Hildo_Jr <93557852+Ooopajovem@users.noreply.github.com>
2025-04-19 13:44:09 +0000

    Desafio_nível _novato

--- xadrez.c
+++ xadrez.c
@@ -5,18 +5,6 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
@@ -27,6 +15,49 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+
+    /*
+    Nível Novato - Movimentação das Peças
+    Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+
+    Implementação de Movimentação do Bispo
+    Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+
+    Implementação de Movimentação da Torre
+    Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+
+    Implementação de Movimentação da Rainha
+    Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    */
+
+        //torre indo para direita
+        printf("Torre\n");
+        for (int i = 0; i < 5; i++)
+        {
+            printf("%d° Direita\n", i+1);
+        }
+        //Bispo indo para diagonal
+        printf("Bispo\n");
+        int pos = 0;
+        while (pos < 5)
+        {
+            if(pos%2 == 0){
+                printf("%d° Cima\n", pos+1);
+            }else{
+                printf("%d° Direita\n", pos+1);
+            }
+            pos = pos +1;
+        }
+        //Rainha indo para esquerda
+        printf("Rainha\n");
+        int ra = 0;
+        do
+        {
+            printf("%d° Esquerda\n",ra);
+            ra = ra + 1;
+        } while (ra <= 8);
+        
+
 
     return 0;
 }
